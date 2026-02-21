b8f93fde60159412bde4e6fdc566a867b0131021
hardik jain <ophardik001@gmail.com>
2026-02-22 04:46:48 +0530

    Fix duplicate style prop in BentoGrid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -69,7 +69,6 @@ export const BentoGridItem = ({
   return (
     <motion.div
       ref={ref}
-      style={motionStyle}
       {...handlers}
       className={cn(
         "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
